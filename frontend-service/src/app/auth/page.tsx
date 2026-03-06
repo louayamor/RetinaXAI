d5d657922d24d88ff511000bb8d1e89794160358
louayamor <amor.louay20@gmail.com>
2026-03-06 16:50:33 +0100

    chore: initialize RetinaXAI frontend service from next-shadcn-dashboard-starter.

---
+++ frontend-service/src/app/auth/page.tsx
@@ -0,0 +1,15 @@
+import { Metadata } from 'next';
+import { redirect } from 'next/navigation';
+
+export const metadata: Metadata = {
+  robots: {
+    index: false,
+    follow: false
+  }
+};
+
+export default function AuthPage() {
+  redirect('/auth/sign-in');
+}
+
+
