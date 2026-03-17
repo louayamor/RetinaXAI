2788bac09ed726848839faf093c9e21e9a3b9a8a
louayamor <amor.louay20@gmail.com>
2026-03-18 00:04:19 +0100

    feat: add constants

---
+++ mlops-service/mlops-service/app/constants/constants.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+CONFIG_FILE_PATH = Path("config/config.yaml")
+PARAMS_FILE_PATH = Path("config/params.yaml")
+SCHEMA_FILE_PATH = Path("config/schema.yaml")
+
+DL_ARTIFACTS_DIR = Path("artifacts/model/dl")
+ML_ARTIFACTS_DIR = Path("artifacts/model/ml")
+DL_DATA_DIR = Path("artifacts/data/processed/dl")
+ML_DATA_DIR = Path("artifacts/data/processed/ml")
+MONITORING_DIR = Path("artifacts/monitoring")
